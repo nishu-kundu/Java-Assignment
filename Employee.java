b20174ebfb83b51837f51429160f822f2871a806
nishu-kundu <nishukundu62@gmail.com>
2025-03-30 22:58:39 +0530

    Add files via upload
     Practice working with various data types, access modifiers, and type casting for error resolution.

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private int id ;
+    private int age =  (int)35.5; // cast operator used . Syntax: (datatype name)
+    private String name ;
+    private  boolean isPermanent;
+    public static void main(String[]args)
+    {
+        System.out.println("Successful started");
+    }
+
+
+
+
+
+}
