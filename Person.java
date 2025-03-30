6afdaa715f9575a820bda856aaee5bc3bbd0c887
nishu-kundu <nishukundu62@gmail.com>
2025-03-30 23:21:10 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+package com.anudip.learning;
+
+public class Person {
+     String name="Nishu";
+    int age=20;
+    int salary=20000;
+   public static void main(String[]args)
+   {
+       System.out.println(" Test Successful");
+   }
+
+}
