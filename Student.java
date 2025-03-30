518e55cbda11ce37bcd5e837dea146a7dd90d940
nishu-kundu <nishukundu62@gmail.com>
2025-03-30 22:45:54 +0530

    Add files via upload
    Introduce basic Java syntax, class creation and member variables.

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+   private  String studentName;
+   private String collegeName;
+   private  int studentId;
+   public static void main(String[]args)
+   {
+    System.out.println("Successful");
+
+   }
+
+
+}
